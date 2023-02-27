a97b3c51fc23e4905b5d6e902e44ecea5295227e
алишер <aliser@MacBook-Air-aliser.local>
2023-02-27 04:31:36 -0800

    Add new Homework 1

---
+++ Home1/Program.cs
@@ -0,0 +1,21 @@
+﻿//Homework1
+
+Console.Write("Введите первое число:");
+int First_Num = int.Parse(Console.ReadLine()??"");
+
+Console.Write("Введите второе число:");
+int Second_Num = int.Parse(Console.ReadLine()??"");
+
+if( First_Num < Second_Num){
+     Console.WriteLine($"{"первое число"} -> {Second_Num} -> {"Больше чем второе число"}{First_Num}");
+}
+else {
+   Console.WriteLine($"{"второе число"}{First_Num} -> {"Больше чем первое число"} -> {Second_Num}");
+    
+}
+
+
+    
+
+
+
