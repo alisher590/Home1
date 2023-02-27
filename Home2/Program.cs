bfd2b53e4b5ce1afa0a8d08583bed23987ab33aa
алишер <aliser@MacBook-Air-aliser.local>
2023-02-27 05:46:17 -0800

    Add new Homework 2

---
+++ Home2/Program.cs
@@ -0,0 +1,22 @@
+﻿// Homework 
+Console.Write("Введите первое число:");
+int First_Num = int.Parse(Console.ReadLine() ?? "");
+
+Console.Write("Введите второе число:");
+int Second_Num = int.Parse(Console.ReadLine() ?? "");
+
+Console.Write("Введите третье число:");
+int Third_Num = int.Parse(Console.ReadLine() ?? "");
+
+int maximum = First_Num;
+if (maximum < Second_Num)
+{
+    maximum = Second_Num;
+    if (maximum < Third_Num)
+    {
+        maximum = Third_Num;
+    }
+}
+Console.WriteLine(maximum);
+
+
